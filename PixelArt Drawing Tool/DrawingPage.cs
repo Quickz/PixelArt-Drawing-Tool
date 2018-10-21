22be6227280782e5a9c9ad3c7651c6239129f2bd
Quickz <alexserg@inbox.lv>
2018-10-21 19:07:30 +0300

    added some code to start with

---
+++ PixelArt Drawing Tool/DrawingPage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PixelArt_Drawing_Tool
+{
+    class DrawingPage
+    {
+        public WriteableBitmap Source { get; private set; }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        private int stride;
+
+        public DrawingPage(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            Create(width, height);
+        }
+
+        private void Create(int width, int height)
+        {
+            // defining parameters
+            PixelFormat format = PixelFormats.Bgra32;
+            stride = (width * format.BitsPerPixel + 7) / 8;
+            byte[] rawImage = new byte[stride * height];
+            
+            // creating a bitmap
+            BitmapSource bitmap = BitmapSource.Create(
+                width,
+                height,
+                96,
+                96,
+                format,
+                null,
+                rawImage,
+                stride);
+
+            Source = new WriteableBitmap(bitmap);
+        }
+
+        /// <summary>
+        /// Draws a pixel on the specified
+        /// location in the page.
+        /// </summary>
+        public void Draw(int x, int y, Color color)
+        {
+            byte[] colorData = { color.B, color.G, color.R, color.A };
+            Int32Rect rect = new Int32Rect(x, y, 1, 1);
+            Source.WritePixels(rect, colorData, stride, 0);
+        }
+    }
+}
---
+++ PixelArt Drawing Tool/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PixelArt_Drawing_Tool
+{
+    /// <summary>
+    ///  Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DrawingPage page;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            page = new DrawingPage(16, 16);
+            PageContainer.Source = page.Source;
+        }
+
+        /// <summary>
+        /// Draws a square at
+        /// the specified position.
+        /// </summary>
+        private void Draw(int x, int y, Color color)
+        {
+            // position outside grid
+            if (x >= page.Width ||
+                y >= page.Height ||
+                x < 0 ||
+                y < 0)
+            {
+                return;
+            }
+
+            page.Draw(x, y, color);
+        }
+
+        private void PageContainer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point position = e.GetPosition(PageContainer);
+
+            // size of single pixel
+            double pixelWidth = PageContainer.ActualWidth / page.Width;
+            double pixelHeight = PageContainer.ActualHeight / page.Height;
+
+            int column = (int)Math.Floor(position.X / pixelWidth);
+            int row = (int)Math.Floor(position.Y / pixelHeight);
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Draw(column, row, Colors.Red);
+            }
+
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                Draw(column, row, Colors.Transparent);
+            }
+        }
+
+        private void PageContainer_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void PageContainer_Loaded(object sender, RoutedEventArgs e)
+        {
+            // making sure background size matches the draw area
+            PageBackground.Width = PageContainer.ActualWidth;
+            PageBackground.Height = PageContainer.ActualHeight;
+        }
+    }
+}
