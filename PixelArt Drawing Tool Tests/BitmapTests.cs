07c67385c2354318afc8f9f2e4bb4caeba5a6623
Quickz <alexserg@inbox.lv>
2019-05-09 23:43:05 +0300

    added a couple of a new tests for the bitmap

--- PixelArt Drawing Tool Tests/BitmapTests.cs
+++ PixelArt Drawing Tool Tests/BitmapTests.cs
@@ -25,5 +25,34 @@ namespace PixelArt_Drawing_Tool_Tests
             Assert.AreEqual(image.Source.PixelWidth, 16);
             Assert.AreEqual(image.Source.PixelHeight, 16);
         }
+
+        [Test]
+        public void IsTransparentByDefault()
+        {
+            var image = new Bitmap(16, 16, format);
+            Color[,] colors = image.AllPixelColors();
+            bool allColorsTransparent = true;
+            for (int i = 0; i < colors.GetLength(0); i++)
+            {
+                for (int j = 0; j < colors.GetLength(1); j++)
+                {
+                    if (colors[i, j].A != 0)
+                    {
+                        allColorsTransparent = false;
+                        break;
+                    }
+                }
+            }
+            Assert.AreEqual(true, allColorsTransparent);
+        }
+
+        [Test]
+        public void ReadAndWriteSinglePixel()
+        {
+            var image = new Bitmap(32, 32, format);
+            image.WritePixel(25, 25, Colors.Black);
+            Color color = image.PixelColorAt(25, 25);
+            Assert.AreEqual(Colors.Black, color);
+        }
     }
 }
